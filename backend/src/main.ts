439b078ceb5d75ee27a4c23b8c8bda37068ccb7a
ggred0123 <ggred0123@gmail.com>
2024-12-01 19:29:00 +0900

    배포되게 main 수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -55,6 +55,10 @@ async function bootstrap() {
   // NestExpressApplication 타입 지정
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // CORS 및 쿠키 파서 설정
+  app.enableCors();
+  app.use(cookieParser());
+
   // class-validator 설정
   app.useGlobalPipes(
     new ValidationPipe({
@@ -68,16 +72,12 @@ async function bootstrap() {
   // 전역 예외 필터 설정
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  // CORS 및 쿠키 파서 설정
-  app.enableCors();
-  app.use(cookieParser());
-
   // 정적 자산 제공 설정 추가
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
 
-  // Swagger 설정
+  // Swagger 설정을 라우트 설정 전에 배치
   const config = new DocumentBuilder()
     .setTitle('Eventory Server')
     .setDescription('Eventory API description')
@@ -89,7 +89,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.PORT || 3000; // 기본값은 3000
-  await app.listen(port);
+  // 기본 라우트를 Swagger 설정 후에 추가
+  const httpAdapter = app.getHttpAdapter().getInstance();
+  httpAdapter.get('/', (req, res) => {
+    res.redirect('/docs'); // Swagger 문서로 리다이렉트
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
