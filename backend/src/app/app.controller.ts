c53703a24d268aac6e96d5bd75e742fac36fc5d3
ggred0123 <ggred0123@gmail.com>
2024-12-30 13:19:11 +0900

    병합?

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -3,13 +3,6 @@ import { AppService } from './app.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
-@ApiTags('Eventory')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('health')
-  @ApiOperation({ summary: 'Health check' })
-  getHello(): string {
-    return this.appService.healthCheck();
-  }
 }
